a315d3e13e36c587fa79b212199bcae47c4e47ee
s8983 <s8983@DESKTOP-QBC14O7>
2023-12-10 19:31:45 +0700

    Добавьте файлы проекта.

---
+++ laba 8.1/laba 8.1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <locale>
+#include <string>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "rus");
+
+    char number[50];
+    cout << "Введите число: ";
+    cin >> number;
+
+    int len = strlen(number);
+    char min = number[0];
+    char max = number[0];
+
+    for (int i = 0; i < len; i++) {
+        if (max < number[i]) {
+            max = number[i];
+        }
+        if (min > number[i]) {
+            min = number[i];
+        }
+    }
+    
+    cout << "Минимальная цифра числа: " << min << endl;
+    cout << "Максимальная цифра числа: " << max;
+}
+
